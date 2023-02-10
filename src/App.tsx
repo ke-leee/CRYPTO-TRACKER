264341011cbc830695db6fdf814ed04fefc8dfd4
kelee <kelee@keleeui-MacBookAir.local>
2023-02-10 22:48:01 +0900

    react query two

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Router from "./Router";
 import GlobalStyle from "./components/GlobalStyle";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 }
